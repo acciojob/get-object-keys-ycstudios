aeefdf185bb24377fc32d7c7b469f6e30ab984b5
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-10 17:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Create a student object with a name property
+// Create the student object
 const student = {
-    name: "John"
+  name: "John"
 };
 
+// Function to get all keys from the object
 function getKeys(obj) {
-    return Object.keys(obj);
+  return Object.keys(obj);
 }
+
 
 // Example usage
 console.log(getKeys(student));  // ["name"]
