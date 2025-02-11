638097a3e8141f856267a4f2a028356adba7a7e7
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 11:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Create test objects
 const student = {
   name: "yash",
+  getKeys: function () {
+    return Object.keys(this);
+  },
 };
 
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-console.log(getKeys(student)); 
+console.log(student.getKeys());
