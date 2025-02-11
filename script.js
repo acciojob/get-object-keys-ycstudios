34cc313e31b8ae05bcf5042b073d315e6c37ba31
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 11:38:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const student = {
   name: "yash"
 };
 
-Object.prototype.getKeys=function () {
+// Adding getKeys() to Object prototype
+Object.prototype.getKeys = function () {
     return Object.keys(this);
+};
+
+// Calling the method
 console.log(student.getKeys());
