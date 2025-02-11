00018fd513a4ffcd847f1394507a417bf81f8faf
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-11 09:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-
-const student = {
-  name: "John"
+// Create test objects
+const student1 = {
+  name: "John",
+  age: 20,
+  city: "New York"
 };
 
+const student2 = {
+  name: "Jane"
+};
+
+// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
-
